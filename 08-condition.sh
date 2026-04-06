c5fa66fbe371f4a66652d05e2355fa687383bcda
Imran <imrankhanmohammed130@gmail.com>
2026-04-05 22:03:24 -0500

    learn shllzjjl

--- 08-condition.sh
+++ 08-condition.sh
@@ -1,4 +1,11 @@
-a=9
-if [ $a -eq 10 ]; then
-  echo "a is 10"
+#a=10
+#if [ $a -eq 10 ]; then
+#  echo "a is 10"
+#fi
+
+a=10
+if [ $a -gt 20 ]; then
+  echo "a is greater than 20"
+else
+  echo "a is not greater than 20"
 fi
