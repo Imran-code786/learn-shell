220d845e31c9d9ef758153e38f27ae49e0ef03b0
Imran <imrankhanmohammed130@gmail.com>
2026-04-05 20:56:56 -0500

    learn shllzjjl

--- 03-redirector.sh
+++ 03-redirector.sh
@@ -1,3 +1,6 @@
 # 1> : Redirect stdout to a file (overwrite)
 echo "Hello" 1>/tmp/output.txt
 echo "Hello" >/tmp/output1.txt
+
+# 2> : Redirect stderr to a file (overwrite)
+ls /nonexistent 2>/tmp/error.txt
